1a365c65e06eb9c28179e6297c6f7193078eaff4
rini <rinioctaviani.com>
2026-06-10 23:53:23 +0800

    pertemuan 4: CRUD

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -2,13 +2,14 @@
 
 
     <x-slot:title>{{ $title }}</x-slot>
-    <form method="POST" action="{{ route('student.store') }}">
+    <form method="POST" action="{{ route('student.update', $student) }}">
         @csrf
+        @method('PUT')
 
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
-                value="{{ old('name') }}">
+                value="{{ old('name', $student->name) }}">
         </div>
         @error('name')
             <div class="alert alert-danger">{{ $message }}</div>
@@ -17,7 +18,7 @@
         <div class="mb-3">
             <label for="nim" class="form-label">NIM</label>
             <input type="number" class="form-control @error('nim') is-invalid @enderror" id="nim" name="nim"
-                value="{{ old('nim') }}">
+                value="{{ old('nim', $student->nim) }}">
         </div>
         @error('nim')
             <div class="alert alert-danger">{{ $message }}</div>
@@ -28,5 +29,5 @@
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
-    <h1 class="fw-bold">Form Create Student</h1>
+
 </x-app>
